856a95c3cce15de4cf17fea0c82d6fd58775b973
CS-LucasGuimaraes <cs.lucas.guimaraes@gmail.com>
2023-11-11 17:25:51 -0300

    commit pattern

---
+++ Homework 1/B - Smallest on the Stack.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int len;
+    stack<string> gifts;
+    string action;
+
+    for (int i = 0; i < len; i++) {
+        
+        cin >> action;
+        cout << action;
+
+
+    }
+}
