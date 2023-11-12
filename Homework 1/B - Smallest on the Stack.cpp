f55fc4d171aacd5725933fc006233f33f182dd8e
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-11-11 23:55:57 -0300

    Update B - Smallest on the Stack.cpp

--- Homework 1/B - Smallest on the Stack.cpp
+++ Homework 1/B - Smallest on the Stack.cpp
@@ -3,15 +3,50 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     int len;
-    stack<string> gifts;
+    stack<int> gifts;
+    stack<int> min_stack;
     string action;
+
+    cin >> len;
 
     for (int i = 0; i < len; i++) {
         
         cin >> action;
-        cout << action;
+        
+        if(action == "PUSH"){
+            int x;
+            cin >> x;
+            gifts.push(x);
 
+            if (min_stack.empty() == true) {
+                min_stack.push(x);
+            }
+            else if (min_stack.top() >= x) {
+                min_stack.push(x);
+            }
+        }
 
+        if(action == "POP") {
+            if (gifts.empty() == true) cout << "EMPTY\n";
+            else if (gifts.top() == min_stack.top()){
+                min_stack.pop();
+                gifts.pop();
+            }
+            else gifts.pop();
+        }
+    
+        if(action == "MIN") {
+            if (gifts.empty() == true) cout << "EMPTY\n";
+            else{
+                cout << min_stack.top() << "\n";
+            }
+        }
     }
+    // if (gifts.empty() == false) cout << gifts.top();
+    
+    return 0;
 }
