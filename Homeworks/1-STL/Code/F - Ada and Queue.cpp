7a721bbe0351497c8a257904facbe87401738d9b
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-11-16 10:16:00 -0300

    Update F - Ada and Queue.cpp

--- Homeworks/1-STL/Code/F - Ada and Queue.cpp
+++ Homeworks/1-STL/Code/F - Ada and Queue.cpp
@@ -9,52 +9,75 @@ int main(){
     deque<int> toDo, reverse_toDo;
     int len, x;
     string action;
+    int isReverse = 0;
 
     cin >> len;
 
     for (int i = 0; i < len; i++)
     {
         cin >> action;
-        if (action == "push_back") {
-            cin >> x;
-            toDo.push_back(x);
-        }
-
-        else if (action == "toFront") {
-            cin >> x;
-            toDo.push_front(x);
-        }
-
-        else if (action == "back") { 
-            if (toDo.empty() == false) {
-                cout << toDo.back() << "\n";
-                toDo.pop_back();
+        if (isReverse == 0) {
+            if (action == "push_back") {
+                cin >> x;
+                toDo.push_back(x);
             }
-            else cout << "No job for Ada?" << "\n";
-        }
 
-        else if (action == "front") { 
-            if (toDo.empty() == false) {
-                cout << toDo.front() << "\n";
-                toDo.pop_front();
+            else if (action == "toFront") {
+                cin >> x;
+                toDo.push_front(x);
             }
-            else cout << "No job for Ada?" << "\n";
+
+            else if (action == "back") { 
+                if (toDo.empty() == false) {
+                    cout << toDo.back() << "\n";
+                    toDo.pop_back();
+                }
+                else cout << "No job for Ada?" << "\n";
+            }
+
+            else if (action == "front") { 
+                if (toDo.empty() == false) {
+                    cout << toDo.front() << "\n";
+                    toDo.pop_front();
+                }
+                else cout << "No job for Ada?" << "\n";
+            }
+            
+            else if (action == "reverse") {
+                isReverse = 1;
+            }
         }
         
-        // 2x linear
-        else if (action == "reverse") {
-            // if (toDo.empty() == false) {
-            //     while (toDo.empty() == false) {
-            //         reverse_toDo.push_front(toDo.front());
-            //         toDo.pop_front();
-            //     }
-                
-            //     while (reverse_toDo.empty() == false) {
-            //         toDo.push_front(reverse_toDo.back());
-            //         reverse_toDo.pop_back();
-            //     }
-            // }
-            reverse(toDo.begin(), toDo.end());
+        else if (isReverse == 1) {
+            if (action == "push_back") {
+                cin >> x;
+                toDo.push_front(x);
+            }
+
+            else if (action == "toFront") {
+                cin >> x;
+                toDo.push_back(x);
+            }
+
+            else if (action == "back") { 
+                if (toDo.empty() == false) {
+                    cout << toDo.front() << "\n";
+                    toDo.pop_front();
+                }
+                else cout << "No job for Ada?" << "\n";
+            }
+
+            else if (action == "front") { 
+                if (toDo.empty() == false) {
+                    cout << toDo.back() << "\n";
+                    toDo.pop_back();
+                }
+                else cout << "No job for Ada?" << "\n";
+            }
+            
+            else if (action == "reverse") {
+                isReverse = 0;
+            }
         }
     }
     
