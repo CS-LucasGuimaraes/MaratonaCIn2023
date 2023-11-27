2054c06e74ae67c7bfd98749054dfbceede216d0
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-11-26 22:12:07 -0300

    tle test 3

---
+++ Homeworks/3-Grafos/Code/G - The Lakes.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int test_cases;
+
+    cin >> test_cases;
+
+    for (int tc = 0; tc < test_cases; tc++) {
+        
+        int row, collumn;
+        int x;
+
+        cin >> row; cin >> collumn;
+
+        int board[row][collumn];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < collumn; j++) {
+                cin >> x;  
+                board[i][j] = x; 
+            }
+        }
+
+        // vector<int> lakes;
+        int maxlake = 0;
+        map<pair<int,int>, int> visited_cords_global;
+
+
+        pair<int,int> notvis = {0,0};
+        while (1) {
+            int actual_row;
+            int actual_col;
+            
+            queue<pair<int,int>> operation_order;
+            pair<int,int> cord;
+            map<pair<int,int>, int> visited_cords;
+            int cont = 0;
+
+            operation_order.push(notvis);
+            visited_cords[notvis] = true;
+            visited_cords_global[notvis] = true;
+            
+
+            while (!operation_order.empty()) {
+                cord = operation_order.front();
+                operation_order.pop();
+                actual_row = cord.first;
+                actual_col = cord.second;
+                cont+=board[actual_row][actual_col];
+
+                if (board[actual_row][actual_col] != 0) {
+                    if (actual_row - 1 >= 0 && visited_cords.find({actual_row-1,actual_col}) == visited_cords.end()) {
+                        operation_order.push({actual_row-1,actual_col});
+                        visited_cords[{actual_row-1,actual_col}] = true;
+                        visited_cords_global[{actual_row-1,actual_col}] = true;
+                    }
+                    if (actual_row + 1 < row && visited_cords.find({actual_row+1,actual_col}) == visited_cords.end()) {
+                        operation_order.push({actual_row+1,actual_col});
+                        visited_cords[{actual_row+1,actual_col}] = true;
+                        visited_cords_global[{actual_row+1,actual_col}] = true;
+                    }
+                    if (actual_col - 1 >= 0 && visited_cords.find({actual_row,actual_col-1}) == visited_cords.end()) {
+                        operation_order.push({actual_row,actual_col-1});
+                        visited_cords[{actual_row,actual_col-1}] = true;
+                        visited_cords_global[{actual_row,actual_col-1}] = true;
+                    }
+                    if (actual_col + 1 < collumn && visited_cords.find({actual_row,actual_col+1}) == visited_cords.end()) {
+                        operation_order.push({actual_row,actual_col+1});
+                        visited_cords[{actual_row,actual_col+1}] = true;
+                        visited_cords_global[{actual_row,actual_col+1}] = true;
+                    }
+                }
+                // else {
+                    
+                // }
+            }
+            // lakes.push_back(cont);
+            if (cont > maxlake) maxlake = cont;
+
+            bool flag = false;
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < collumn; j++) {
+                    if (visited_cords_global.find({i,j}) == visited_cords_global.end()) {
+                        notvis = {i,j};
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) break;
+                
+            }
+            if (!flag) break;
+            
+        }
+
+        cout << maxlake << '\n';
+    }
+
+    return 0;
+}
