7b44db2bd5511964388dbe5427296d92b116d5a4
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-11-28 13:05:52 -0300

    H - WA - Test 2

--- Homeworks/3-Grafos/Code/H - Jumping.cpp
+++ Homeworks/3-Grafos/Code/H - Jumping.cpp
@@ -8,14 +8,14 @@ int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    freopen("jumping.in", "r", stdin);
-   //freopen("*.out", "w", stdout);
+    // freopen("jumping.in", "r", stdin);
+    // freopen("*.out", "w", stdout);
 
     int test_cases;
 
     cin >> test_cases;
 
-    for (int tc = 0; tc < test_cases; tc++) {
+    for (int tc = 0; tc <    test_cases; tc++) {
         int shoppings, x;
         cin >> shoppings;
         vector<vector<int>> adj(shoppings+1);
@@ -26,32 +26,32 @@ int32_t main(){
             if (i+x <= shoppings) adj[i].push_back(i+x);
         }
 
-        for (int i = 1; i < shoppings+1; i++) {
-            queue<int> operation_order;
-            vector<bool> visited(shoppings+1, false);
-            vector<int> dist(shoppings+1, 10e5+2);
+        queue<int> operation_order;
+        vector<bool> visited(shoppings+1, false);
+        vector<int> dist(shoppings+1, 10e5+2);
 
-            operation_order.push(i);
-            visited[i] = true;
-            dist[i] = 0;
-            while (!operation_order.empty()) {
-                int j = operation_order.front();
-                operation_order.pop();
-                vector<int> k = adj[j];
-                for (auto e:k) {
-                    if (!visited[e]) {
-                        operation_order.push(e);
-                        if (dist[e] > dist[j] + 1) dist[e] = dist[j] + 1;
-                        visited[e] = true;
-                    }
+        int i = shoppings;
+        operation_order.push(i);
+        visited[i] = true;
+        dist[i] = 0;
+        while (!operation_order.empty()) {
+            int j = operation_order.front();
+            operation_order.pop();
+            vector<int> k = adj[j];
+            for (auto e:k) {
+                if (!visited[e]) {
+                    operation_order.push(e);
+                    if (dist[e] > dist[j] + 1) dist[e] = dist[j] + 1;
+                    visited[e] = true;
                 }
             }
-            if (dist[shoppings] != 10e5+2) cout << dist[shoppings] << '\n';
+        }
+
+        for (int i = 1; i < shoppings+1; i++) {
+            if (dist[i] != 10e5+2) cout << dist[i] << '\n';
             else cout << -1 << '\n';
         }
-        
     }
-    
 
     return 0;
 }
