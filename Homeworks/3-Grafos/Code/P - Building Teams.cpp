49b3b17f48f073c6174a3443449de4747649df95
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-01 10:48:19 -0300

    P - Accepted

--- Homeworks/3-Grafos/Code/P - Building Teams.cpp
+++ Homeworks/3-Grafos/Code/P - Building Teams.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+vector<vector<int>> adj(10e5+2);
+vector<bool> colored(10e5+2, false);
+vector<bool> color(10e5+2, false);
+
+
+bool bipart(int e) {
+    colored[e] = true;
+    for(auto u:adj[e]) {
+        if (!colored[u]) {
+            color[u] = !color[e];
+            colored[u] = true;
+            if (!bipart(u)) return false;
+        }
+        else if (color[e] == color[u]) return false;
+    }
+    return true;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -11,7 +29,6 @@ int main(){
 
     cin >> people; cin >> friendship;
 
-    vector<vector<int>> adj(people+1);
 
     for (int i = 0; i < friendship; i++) {
         cin >> x; cin >> y;
@@ -19,40 +36,20 @@ int main(){
         adj[y].push_back(x);
     }
 
-    queue<int> operation_order;
-    vector<bool> visited(people+1, false);
-    vector<int> team(people+1, 0);
 
-    for (int j = 1; j < people+1; j++)
-    {
-        if (!visited[j]) {
-            operation_order.push(j);
-            visited[j] = true;
-            team[j] = 1;
-        // cout << "AQUI!" << endl;
-            while (!operation_order.empty()) {
-                int i = operation_order.front();
-                operation_order.pop();
-
-                for (auto e:adj[i]) {
-                    if (!visited[e]) {
-                        if (team[i] == 1) team[e] = 2;
-                        else team[e] = 1;
-                        operation_order.push(e);
-                        visited[e] = true;
-                    }
-                    if (visited[e] && team[i] == team[e]) {
-                        cout << "IMPOSSIBLE";
-                        return 0;
-                    }
-                }
-            }
+    for (int i = 1; i < people+1; i++) {
+        if (!colored[i]) {
+            if (!bipart(i)) {
+                cout << "IMPOSSIBLE";
+                return 0;
+            }   
         }
     }
 
-
     for (int i = 1; i < people+1; i++) {
-        cout << team[i] << " ";
+        if (!color[i]) cout << "1 ";
+        else cout << "2 ";
     }
+
     return 0;
-}
+}       
