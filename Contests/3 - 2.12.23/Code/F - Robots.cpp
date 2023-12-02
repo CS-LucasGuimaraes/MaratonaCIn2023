cc97b8a56f2ecf3a2a3e12cff6c14ba76443dee6
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-02 09:26:41 -0300

    F - accepted

--- Contests/3 - 2.12.23/Code/F - Robots.cpp
+++ Contests/3 - 2.12.23/Code/F - Robots.cpp
@@ -16,13 +16,10 @@ int main(){
 
         cin >> row; cin >> col;
 
-        // char board[row][col];
         char x;
 
         vector<int> top, lft; 
-        // vetor<int> bot,  rgt;
         int mintop = row , minlft = col;
-        // int minbot = 6, minrgt=6;
 
         for (int i = 0; i < row; i++) {
             cin.get();
@@ -31,14 +28,10 @@ int main(){
                 
                 if (x == 'R') {
                     top.push_back(i);
-                    // bot.push_back(row - i-1);
                     lft.push_back(j);
-                    // rgt.push_back(col - j-1);
 
                     if (i < mintop) mintop = i;
-                    // if (row-i-1 < minbot) minbot = row-i-1;
                     if (j < minlft) minlft = j;
-                    // if (col-j-1 < minrgt) minrgt = col-j-1;
                 }
             }
         }
@@ -58,10 +51,7 @@ int main(){
         if (!bp) {
             cout << "NO" << '\n';
         }
-        
-
     }
-    
 
     return 0;
 }
