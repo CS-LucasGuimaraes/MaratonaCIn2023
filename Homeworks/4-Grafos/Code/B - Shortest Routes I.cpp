03a3ba2b7420c35af13fb16e6e1015acc39e2dab
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-04 22:12:36 -0300

    B - Accepted

--- Homeworks/4-Grafos/Code/B - Shortest Routes I.cpp
+++ Homeworks/4-Grafos/Code/B - Shortest Routes I.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 
 #define ll long long
+#define int long long
 
 using namespace std;
 
-int main(){
+int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
