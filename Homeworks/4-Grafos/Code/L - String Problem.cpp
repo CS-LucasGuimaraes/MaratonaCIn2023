be334674edf7991fb59bb892bcd93114e120cabe
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-06 10:41:54 -0300

    L - Accepted

--- Homeworks/4-Grafos/Code/L - String Problem.cpp
+++ Homeworks/4-Grafos/Code/L - String Problem.cpp
@@ -1,28 +1,40 @@
 #include <bits/stdc++.h>
 
-#define INF 10e5
+#define INF 2601
 
 using namespace std;
 
-int main(){
+int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     string s, t; 
     
     cin >> s >> t;
-
     int changes; cin >> changes;
     
     char a, b;
     int weight;
 
-    vector<vector<pair<int,int>>> adj(97+26);
+    vector<vector<int>> matrix(26, vector<int>(26, INF));
 
     for (int i = 0; i < changes; i++) {
         cin >> a >> b; cin >> weight;
+        a -= 97; b -= 97;
 
-        adj[a].push_back({weight, b});
+        matrix[a][b] = min(matrix[a][b], weight);
+    }
+
+    for (int i = 0; i < 26; i++) {
+        matrix[i][i] = 0;
+    }
+    
+    for (int k = 0; k < 26; k++) {
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                matrix[i][j] = min(matrix[i][j], matrix[i][k]+matrix[k][j]);
+            }
+        }
     }
 
     if (s.size() != t.size()) {
@@ -33,67 +45,38 @@ int main(){
     int len = s.size();
 
     string answer;
-    int sum = 0; 
-    int c; 
+    int sum = 0;
 
     for (int i = 0; i < len; i++) {
-        a = s[i];
-        b = t[i];
-
-        if (a == b) answer.push_back(a);
-        else {
-            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> operation_order;
-            vector<vector<pair<int,int>>> total_weight(2, vector<pair<int,int>>(97+26, pair<int,int>(INF, 0)));
-            for (int i = 0; i < 2; i++) {
-
-                operation_order.push({0,a});
-                while(!operation_order.empty()) {
-
-                    int wgt = operation_order.top().first;
-                    int out = operation_order.top().second;
-
-                    operation_order.pop();
-
-                    if (wgt > total_weight[i][out].first) continue;
-
-                    for(auto e: adj[out]) {
-                        int n_wgt = e.first;
-                        int next = e.second;
-                        int current_wgt = total_weight[i][out].first + n_wgt;
-
-                        if (current_wgt < total_weight[i][next].first) {
-                            total_weight[i][next].first = current_wgt;
-                            total_weight[i][next].second = out;
-                            operation_order.push({current_wgt, next});
-                        }
-                    }
-                }
-            }
-
-            // 
-
-            pair<int,int> minimum(10e5,0);
-
-            for (auto i:total_weight[0]) {
-                for (auto j:total_weight[1]) {
-                    if (i.second != j.second) continue;
-
-                    if (min(i.first, j.first) < minimum.first) {
-                        if (i.first < j .first) minimum = i;
-                                           else minimum = j;
-                    }
-                }
-            }
-
-            if (minimum.second == 0) {
-                cout << -1;
-                return 0;
-            }
-
-            answer = minimum.second;
-            sum += minimum.first;
+        if (s[i] == t[i]) {
+            answer.push_back(s[i]);
+            continue;
         }
+
+        a = s[i] - 97;
+        b = t[i] - 97;
+        
+        vector<int> sum_row(26);
+        int min = INF;
+        char min_char;
+
+        for (int j = 0; j < 26; j++) {
+            sum_row[j] = matrix[a][j] + matrix[b][j];
+            if (sum_row[j] < min) {
+                min = sum_row[j];
+                min_char = j + 97;
+            }
+        }
+
+        if (min >= INF) {
+            cout << -1;
+            return 0;
+        }
+
+        answer.push_back(min_char);
+        sum += min;
     }
+    
     
     cout << sum << '\n' << answer;
 
