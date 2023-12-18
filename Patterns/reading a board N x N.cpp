0418a1b234e10c1bc9880312ef2b41d35b8c3050
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-18 00:51:12 -0300

    Adjusting patterns

--- Patterns/reading a board N x N.cpp
+++ Patterns/reading a board N x N.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 
+#define endl '\n'
+
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    char b;
-
+    int n; char c;
+    
     cin >> n;
-
+    
     char board[n][n];
-
+    
     for (int i = 0; i < n; i++) {
         cin.get();
         for (int j = 0; j < n; i++) {
-            cin.get(b);
-            board[i][j] = b;
+            cin.get(c);
+            board[i][j] = c;
         }
     }
-    
 
     return 0;
 }
--- Patterns/reading a board Row x Col.cpp
+++ Patterns/reading a board Row x Col.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 
+#define endl '\n'
+
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int row, col;
-    char b;
-
-    cin >> row; cin >> col;
-
+    int row, col; char c;
+    
+    cin >> row >> col;
+    
     char board[row][col];
-
+    
     for (int i = 0; i < row; i++) {
         cin.get();
         for (int j = 0; j < col; i++) {
-            cin.get(b);
-            board[i][j] = b;
+            cin.get(c);
+            board[i][j] = c;
         }
     }
-    
 
     return 0;
 }
