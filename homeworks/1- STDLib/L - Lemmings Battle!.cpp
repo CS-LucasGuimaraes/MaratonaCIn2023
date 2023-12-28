d8571fcc97026597882a808402c8c6742058a936
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-28 20:48:32 -0300

    L - Upsolved

--- homeworks/1- STDLib/L - Lemmings Battle!.cpp
+++ homeworks/1- STDLib/L - Lemmings Battle!.cpp
@@ -1,87 +1,83 @@
 #include <bits/stdc++.h>
 
+#define endl '\n'
+
 using namespace std;
+
+void solve(){
+    int battlefields, greenSoldiers, blueSoldiers;
+
+    cin >> battlefields >> greenSoldiers >> blueSoldiers;
+
+    priority_queue<int> greenArmy;
+    priority_queue<int> blueArmy;
+
+    int x;
+
+    for (int i = 0; i < greenSoldiers; i++) {
+        cin >> x;
+
+        greenArmy.push(x);
+    }
+
+    for (int i = 0; i < blueSoldiers; i++) {
+        cin >> x;
+
+        blueArmy.push(x);
+    }
+
+    ///
+
+
+    while(!greenArmy.empty() && !blueArmy.empty()){
+        vector<int> actual_green;
+        vector<int> actual_blue;
+
+        int k = 0;
+
+        for (int i = 0; i < battlefields; i++) {
+            if (!greenArmy.empty() && !blueArmy.empty()){
+                actual_green.push_back(greenArmy.top());
+                actual_blue.push_back(blueArmy.top());
+
+                greenArmy.pop(); blueArmy.pop();
+                k++;
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (actual_green[i] > actual_blue[i]) greenArmy.push(actual_green[i]-actual_blue[i]);
+            else if (actual_green[i] < actual_blue[i]) blueArmy.push(actual_blue[i]-actual_green[i]);
+        }
+        
+    }
+
+    if (greenArmy.empty() && blueArmy.empty()) cout << "green and blue died" << endl;
+    else if (!greenArmy.empty()) {
+        cout << "green wins" << endl;
+        while(!greenArmy.empty()) {
+            cout << greenArmy.top() << endl;
+            greenArmy.pop();
+        }
+    }
+    else if (!blueArmy.empty()) {
+        cout << "blue wins" << endl; 
+        while(!blueArmy.empty()) {
+            cout << blueArmy.top() << endl;
+           blueArmy.pop();
+        }
+    }
+
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
-    int len;
 
-    cin >> len;
-
-    for (int i = 0; i < len; i++) {
-        int battlefields, greenNumber, blueNumber, soldier;
-        priority_queue<int> greenArmy, blueArmy;
-
-        cin >> battlefields;
-        cin >> greenNumber;
-        cin >> blueNumber;
-
-        for (int j = 0; j < greenNumber; j++) {
-            cin >> soldier;
-            greenArmy.push(soldier);
-        }
-        for (int j = 0; j < blueNumber; j++) {
-            cin >> soldier;
-            blueArmy.push(soldier);
-        }
-// !!!2 =============================================
-        stack<int> greenFight, blueFight;
-        while (1) {
-            for (int k = 0; k > battlefields; k++) {
-                if (greenFight.empty() == false && blueFight.empty() == false) {
-
-                    greenFight.push(greenArmy.top());
-                    blueFight.push(blueArmy.top());
-                    greenArmy.pop();
-                    blueArmy.pop();
-                }
-            }
-
-    // 3 #############################################
-            for (int k = 0; k > battlefields; k++) {
-                if (greenFight.empty() == false && blueFight.empty() == false) {
-                    if (greenFight.top() > blueFight.top()) {
-                        greenArmy.push(greenFight.top() - blueFight.top());
-                    }
-                    if (greenFight.top() < blueFight.top()) {
-                        blueArmy.push(blueFight.top() - greenFight.top());
-                    }
-                    if (greenFight.top() < blueFight.top()) {
-                    }
-                    greenFight.pop();
-                    blueFight.pop();
-                }
-            }
-    // 4 ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-            if (greenArmy.empty() == true || blueArmy.empty() == true) {
-
-                if (greenArmy.empty() == true && blueArmy.empty() == true) {
-                    cout << "green and blue died\n\n";
-                    break;
-                }
-                else if (greenArmy.empty() == true) {
-                    cout << "blue wins\n";
-                    break;
-                    while (blueArmy.empty() == false) {
-                        cout << blueArmy.top() << '\n';
-                        blueArmy.pop();
-                    }
-                    cout << '\n';
-                }
-                else if (blueArmy.empty() == true) {
-                    cout << "green wins\n";
-                    break;
-                    while (greenArmy.empty() == false) {
-                        cout << greenArmy.top() << '\n';
-                        greenArmy.pop();
-                    }
-                    cout << '\n';
-                }
-            }
-        }
-    return 0;
+    int tc; cin >> tc; while (tc--) {
+        solve();
+        if (tc) cout << '\n';
     }
+
+    return 0;
 }
