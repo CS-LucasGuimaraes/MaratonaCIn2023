1c2cf5c70a385397e16e260711437a862aa2a871
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2024-03-09 11:24:51 -0300

    F - AC

---
+++ contests/9 - 09.03.24/F - Legendary Players.py
@@ -0,0 +1,21 @@
+dict = {
+    'tourist': 3858,
+    'ksun48': 3679,
+    'Benq': 3658,
+    'Um_nik': 3648,
+    'apiad': 3638,
+    'Stonefeang': 3630,
+    'ecnerwala': 3613,
+    'mnbvmar': 3555,
+    'newbiedmy': 3516,
+    'semiexp': 3481,
+}
+
+def main():
+    s = input()
+    print(dict[s])
+
+    return 1
+
+
+main()  # Start the program execution
